ff6b6ecc441e9c95cb35019cba2d01434b5cdb7a
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-09-24 17:43:13 -0500

    Updates

---
+++ precache-manifest.96823c5786961b05ace2a8488556aaea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5702589b8c1c2fa7cadfd8e342a58fed",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "16337bca3e9cd9361008",
+    "url": "/my-portfolio/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6209175a301852f8ac58",
+    "url": "/my-portfolio/static/css/main.7ebf9a11.chunk.css"
+  },
+  {
+    "revision": "16337bca3e9cd9361008",
+    "url": "/my-portfolio/static/js/2.39c9835d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-portfolio/static/js/2.39c9835d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6209175a301852f8ac58",
+    "url": "/my-portfolio/static/js/main.a82d6f98.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "656e540533f536cb1d05267c0c7cca04",
+    "url": "/my-portfolio/static/media/Alan2.656e5405.jpg"
+  },
+  {
+    "revision": "17df57d9f09383f076c949b0194b919f",
+    "url": "/my-portfolio/static/media/background.17df57d9.jpg"
+  }
+]);
