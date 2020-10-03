8eb0376f78e5def48c7cf5398c7e04d95104b895
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-10-03 12:36:01 -0500

    debugging

--- src/components/pages/experience.js
+++ src/components/pages/experience.js
@@ -15,7 +15,7 @@ function Experience() {
                     </div>
                     <div class="col-md-8 experience-card">
                     <div class="card-body experience-card">
-                        <h6 class="card-title experience-text">Ford Motor Company | February 2020 | Junior Engineer</h6>
+                        <h6 class="card-title experience-text"><strong>Ford Motor Company | February 2020 | Junior Engineer</strong></h6>
                         <p class="card-text experience-text">The main role as an engineer was to find a solution for a Key Initiative inside the company in which the main purpose was to create a collaborative platform that allows all employees from Ford Mexico, to develop and share new ideas for the company. Present-day, the platform is still in the development process. Also, this platform has been tested with pilot groups, bigger groups every time to have better feedback and develop the best possible product.  
                         </p>
                     </div>
@@ -29,7 +29,7 @@ function Experience() {
                     </div>
                     <div class="col-md-8 experience-card">
                     <div class="card-body experience-card">
-                        <h6 class="card-title experience-text">Eagleburgmann | June 2018 - May 2019 | Project Engineer</h6>
+                        <h6 class="card-title experience-text"><strong>Eagleburgmann | June 2018 - May 2019 | Project Engineer</strong></h6>
                         <p class="card-text experience-text">The main role as a project engineer was to deliver mechanical seal quotations for the different subsidiaries in Latin America, North America, and Europe, always ensuring good communication. The final cost of the product was obtained by calculating the cost of labor, cost of material, quantity of material, and the manufacturing process. Good communication between the technicians and engineers was key in this process. 
                         </p>
                     </div>
